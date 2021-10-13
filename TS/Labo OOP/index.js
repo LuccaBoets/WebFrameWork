3e58c916d1b5b1c5a20201701d2e64f14825e73e
Lucca Boets <boetslucca@gmail.com>
2021-10-13 13:22:25 +0200

    Added Labo OOP

---
+++ TS/Labo OOP/index.js
@@ -0,0 +1,36 @@
+var GestructureerdeMededeling = /** @class */ (function () {
+    //private _checkSum: number;
+    function GestructureerdeMededeling(jaar, klantNr, bestelNummer) {
+        var _this = this;
+        if (jaar === void 0) { jaar = 0; }
+        if (klantNr === void 0) { klantNr = 0; }
+        if (bestelNummer === void 0) { bestelNummer = 0; }
+        this.toString = function () {
+            return "" + GestructureerdeMededeling.decorator + _this.jaar + GestructureerdeMededeling.separator + _this.klantNr + GestructureerdeMededeling.separator + _this.bestelNummer + _this.getCheckSum() + GestructureerdeMededeling.decorator;
+        };
+        this.jaar = this.validate(jaar, 3);
+        this.klantNr = this.validate(klantNr, 3);
+        this.bestelNummer = this.validate(bestelNummer, 3);
+    }
+    GestructureerdeMededeling.prototype.getCheckSum = function () {
+        return Number("" + this.jaar + this.klantNr + this.bestelNummer) % 97;
+    };
+    GestructureerdeMededeling.prototype.validate = function (val, size) {
+        var temp = String(val);
+        if (temp.length > size) {
+            temp = temp.substring(temp.length - size);
+        }
+        else if (temp.length < size) {
+            var count = size - temp.length;
+            for (var index = 0; index < count; index++) {
+                temp = "0" + temp;
+            }
+        }
+        return temp;
+    };
+    GestructureerdeMededeling.decorator = "+++";
+    GestructureerdeMededeling.separator = "/";
+    return GestructureerdeMededeling;
+}());
+var greeter = new GestructureerdeMededeling(2017, 123, 1);
+console.log(greeter.toString());
---
+++ TS/Labo OOP/index.ts
@@ -0,0 +1,54 @@
+interface IGestructureerdeMededeling {
+    jaar: string;
+    klantNr: string;
+    bestelNummer: string;
+
+    toString();
+
+  }
+
+class GestructureerdeMededeling implements IGestructureerdeMededeling  {
+    jaar: string;
+    klantNr: string;
+    bestelNummer: string;
+    static decorator : string = "+++";
+    static separator : string = "/";
+    //private _checkSum: number;
+   
+    constructor(jaar: number = 0, klantNr: number = 0, bestelNummer: number = 0) {
+      this.jaar = this.validate(jaar, 3);
+      this.klantNr = this.validate(klantNr, 3);
+      this.bestelNummer = this.validate(bestelNummer, 3);
+    }
+
+    getCheckSum() {
+        return Number(`${this.jaar}${this.klantNr}${this.bestelNummer}`) % 97;
+    }
+
+
+    validate(val: number, size: number) {
+
+        var temp: string = String(val);
+    
+        if (temp.length > size) {
+            temp = temp.substring(temp.length - size);
+        } else if (temp.length < size) {
+    
+            var count = size - temp.length;
+            for (let index = 0; index < count; index++) {
+                temp = "0" + temp
+            }
+        }
+    
+        return temp;
+    }
+
+    public toString = () : string => {
+        return `${GestructureerdeMededeling.decorator}${this.jaar}${GestructureerdeMededeling.separator}${this.klantNr}${GestructureerdeMededeling.separator}${this.bestelNummer}${this.getCheckSum()}${GestructureerdeMededeling.decorator}`
+    }
+   
+  }
+   
+  let greeter = new GestructureerdeMededeling(2017, 123, 1);
+
+  console.log(greeter.toString())
